298e9c8d88220185162c500085e99ae1a469a09b
mrsurprise123 <44513517+mrsurprise123@users.noreply.github.com>
2020-04-20 00:13:50 -0400

    original

---
+++ Snake/Assets/Scripts/MainUIController.cs
@@ -0,0 +1,123 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+public class MainUIController : MonoBehaviour
+{
+    private static MainUIController _instance;
+    public static MainUIController Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+
+    public bool hasBorder = true;
+    public bool isPause = false;
+    public int score = 0;
+    public int length = 0;
+    public Text msgText;
+    public Text scoreText;
+    public Text lengthText;
+    public Image pauseImage;
+    public Sprite[] pauseSprites;
+    public Image bgImage;
+    private Color tempColor;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        if (PlayerPrefs.GetInt("border", 1) == 0)
+        {
+            hasBorder = false;
+            foreach (Transform t in bgImage.gameObject.transform)
+            {
+                t.gameObject.GetComponent<Image>().enabled = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        switch (score / 100)
+        {
+            case 0:
+            case 1:
+            case 2:
+                break;
+            case 3:
+            case 4:
+                ColorUtility.TryParseHtmlString("#CCEEFFFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "Level" + 2;
+                break;
+            case 5:
+            case 6:
+                ColorUtility.TryParseHtmlString("#CCFFDBFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "Level" + 3;
+                break;
+            case 7:
+            case 8:
+                ColorUtility.TryParseHtmlString("#EBFFCCFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "Level" + 4;
+                break;
+            case 9:
+            case 10:
+                ColorUtility.TryParseHtmlString("#FFF3CCFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "Level" + 5;
+                break;
+            default:
+                ColorUtility.TryParseHtmlString("#FFDACCFF", out tempColor);
+                bgImage.color = tempColor;
+                msgText.text = "Endless Level";
+                break;
+        }
+    }
+
+    public void UpdateUI(int s = 5, int l = 1)
+    {
+        score += s;
+        length += l;
+        scoreText.text = "Score:\n" + score;
+        lengthText.text = "Length:\n" + length;
+    }
+
+    public void Pause()
+    {
+        isPause = !isPause;
+        if (isPause)
+        {
+            Time.timeScale = 0;
+            pauseImage.sprite = pauseSprites[1];
+        }
+        else
+        {
+            Time.timeScale = 1;
+            pauseImage.sprite = pauseSprites[0];
+        }
+    }
+
+    public void Home()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(0);
+    }
+    public void Restart()
+    {
+        StartCoroutine(Restartgame(1.0f));
+    }
+    IEnumerator Restartgame(float t)
+    {
+        yield return new WaitForSeconds(t);
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+
+}
---
+++ Snake/Assets/Scripts/StartUIController.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartUIController : MonoBehaviour
+{
+    public Text lastText;
+    public Text bestText;
+    public Toggle blue;
+    public Toggle yellow;
+    public Toggle border;
+    public Toggle noBorder;
+
+    void Awake()
+    {
+        lastText.text = "Last：Length" + PlayerPrefs.GetInt("lastl", 0) + "，Score" + PlayerPrefs.GetInt("lasts", 0);
+        bestText.text = "Best：Length" + PlayerPrefs.GetInt("bestl", 0) + "，Score" + PlayerPrefs.GetInt("bests", 0);
+    }
+
+    void Start()
+    {
+        if (PlayerPrefs.GetString("snake_head", "head2") == "head2")
+        {
+            blue.isOn = true;
+            PlayerPrefs.SetString("snake_head", "head2");
+            PlayerPrefs.SetString("snake_body_01", "body_blue_01");
+            PlayerPrefs.SetString("snake_body_02", "body_blue_02");
+        }
+        else
+        {
+            yellow.isOn = true;
+            PlayerPrefs.SetString("snake_head", "head1");
+            PlayerPrefs.SetString("snake_body_01", "body_yellow_01");
+            PlayerPrefs.SetString("snake_body_02", "body_yellow_02");
+        }
+        if (PlayerPrefs.GetInt("border", 1) == 1)
+        {
+            border.isOn = true;
+            PlayerPrefs.SetInt("border", 1);
+        }
+        else
+        {
+            noBorder.isOn = true;
+            PlayerPrefs.SetInt("border", 0);
+        }
+    }
+
+    public void BlueSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            PlayerPrefs.SetString("snake_head", "head2");
+            PlayerPrefs.SetString("snake_body_01", "body_blue_01");
+            PlayerPrefs.SetString("snake_body_02", "body_blue_02");
+        }
+    }
+
+    public void YellowSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            PlayerPrefs.SetString("snake_head", "head1");
+            PlayerPrefs.SetString("snake_body_01", "body_yellow_01");
+            PlayerPrefs.SetString("snake_body_02", "body_yellow_02");
+        }
+    }
+
+    public void BorderSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            PlayerPrefs.SetInt("border", 1);
+        }
+    }
+
+    public void NoBorderSelected(bool isOn)
+    {
+        if (isOn)
+        {
+            PlayerPrefs.SetInt("border", 0);
+        }
+    }
+
+    public void StartGame()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+    }
+}
